bcc11b197434acf3e3274b4bbaf1789c6946131f
ANKITpandey75 <125044396+ANKITpandey75@users.noreply.github.com>
2026-06-29 15:54:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 function updateTimer() {
-    
     var now = new Date();
 
-   
-    document.getElementById("timer").textContent = now.toLocaleString();
+    document.getElementById("timer").textContent = now.toLocaleString("en-IN", {
+        day: "2-digit",
+        month: "2-digit",
+        year: "numeric",
+        hour: "numeric",
+        minute: "2-digit",
+        second: "2-digit",
+        hour12: true
+    });
 }
 
-
 updateTimer();
-
-
-setInterval(updateTimer, 1000);//your JS code here. If required.
+setInterval(updateTimer, 1000);
