5a8975ce5308e9ca40dafe0bd04f1ae605f5bc4e
ANKITpandey75 <125044396+ANKITpandey75@users.noreply.github.com>
2026-06-29 15:48:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-//your JS code here. If required.
+function updateTimer() {
+    
+    var now = new Date();
+
+   
+    document.getElementById("timer").textContent = now.toLocaleString();
+}
+
+
+updateTimer();
+
+
+setInterval(updateTimer, 1000);//your JS code here. If required.
